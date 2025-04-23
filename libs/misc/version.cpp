d34537c735846d6f3613cc4a84d977e72b5fc259
mikc <mikcieslak@users.noreply.github.com>
2025-04-22 23:05:43 -0600

    Updates vlab version number
    Also, fixes small typo in browser/main.cpp

--- libs/misc/version.cpp
+++ libs/misc/version.cpp
@@ -41,12 +41,12 @@ int version_minor() { return 1; }
 int version_minor_minor() { return 0; }
 
 int build_number() {
-  return 3610; // this build number is changed manually 
+  return 3611; // this build number is changed manually 
 }
 
 std::string build_date_string() {
   // Date of build is set manually
-  return "February 28th, 2025";
+  return "April 22nd, 2025";
 }
 
 std::string version_string() {
