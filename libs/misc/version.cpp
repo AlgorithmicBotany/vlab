e9200d3b1d31a1e64adb6195da017f20985f2c0c
mikc <mikcieslak@users.noreply.github.com>
2026-05-05 16:47:06 -0600

    Update version number
    This version can fixes for raserver and vlabd

--- libs/misc/version.cpp
+++ libs/misc/version.cpp
@@ -41,12 +41,12 @@ int version_minor() { return 1; }
 int version_minor_minor() { return 0; }
 
 int build_number() {
-  return 3615; // this build number is changed manually 
+  return 3616; // this build number is changed manually 
 }
 
 std::string build_date_string() {
   // Date of build is set manually
-  return "December 11th, 2025";
+  return "May 5th, 2026";
 }
 
 std::string version_string() {
